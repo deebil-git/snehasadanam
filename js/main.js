a3410f36b8407d9f522c721d7d65096b6c52ba4f
mohamednayif <mohamednayif@gmail.com>
2019-09-26 20:30:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -103,7 +103,7 @@
     * ------------------------------------------------------ */
     const ssFinalCountdown = function() {
 
-        const finalDate = '2019/11/01';
+        const finalDate = '2019/10/10';
 
         $('.counter').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
